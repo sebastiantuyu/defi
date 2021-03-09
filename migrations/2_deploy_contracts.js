1f98ce4ed15b8180808c406332ed2822433fd123
sebastiantuyu <alextpineiro@gmail.com>
2021-03-08 20:09:10 -0600

    Adding functions on smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,19 @@
+const DappToken = artifact.require('DappToken');
+const DaiToken = artifact.require('DaiToken');
 const TokenFarm = artifacts.require('TokenFarm');
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer) {
+    
+    await deployer.deploy(DappToken);
+    const dappToken = await DappToken.deployed();
+
+    await deployer.deploy(DaiToken);
+    const daiToken = await DaiToken.deployed();
+    
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+    const tokenFarm = await TokenFarm.deployed();
+
+    await dappToken.transfer(tokenFarm.address,'1000000000000000000000000');
+
+    await daiToken.transfer(accounts[0],'1000000000000000000000000');
 };
